fba02018dc9f84321026214b3c1b1a114a2d6033
miketeglas@gmail <mihalyteglas@192.168.0.101>
2014-06-26 23:21:12 -0400

    google font style change

--- www/wp-content/themes/classtheme/header.php
+++ www/wp-content/themes/classtheme/header.php
@@ -4,7 +4,7 @@
 <title>Mike's Portfolio</title>
 <link rel="stylesheet" href= "<?php echo get_template_directory_uri()?>/css/lavish-bootstrap.css"/>
 <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
-<link href='//fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Pacifico' rel='stylesheet' type='text/css'>
 </head>
 <body>
 	<div id="wrapper">
