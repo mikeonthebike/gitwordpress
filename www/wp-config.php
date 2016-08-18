ee09ae633db45bb3d5a672b59c462fd892ddb97a
Rich Hildred <rhildred@gmail.com>
2016-08-17 23:16:37 -0400

    debug code???

--- www/wp-config.php
+++ www/wp-config.php
@@ -81,6 +81,7 @@ define('WP_DEBUG', false);
 
 /* That's all, stop editing! Happy blogging. */
 if ($_SERVER['HTTPS'] == "https") $_SERVER['HTTPS']="on";
+print_r($_SERVER);
 /** Absolute path to the WordPress directory. */
 if ( !defined('ABSPATH') )
 	define('ABSPATH', dirname(__FILE__) . '/');
